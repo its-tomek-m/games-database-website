f4265da141324c9c6d857c9ee6e678c74d44b6f6
Tomek M <tomek.madej3@gmail.com>
2025-06-11 13:12:36 +0200

    Initialize a repo wiht m new page. feat: Add new fields to dummy data, update formatting logic, setup initial structure for new pages and README

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./about.html",
+    "./sources.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Catching all JS files in src and its subdirectories
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
